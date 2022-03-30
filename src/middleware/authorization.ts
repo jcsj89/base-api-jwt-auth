6031538b938dfa79fee3bc30335605717760228b
Jose Carlos Sant Anna Junior <jcsj2010@gmail.com>
2022-03-29 21:11:24 -0300

    29.03.22
    Minimo ridiculo possivel

--- src/middleware/authorization.ts
+++ src/middleware/authorization.ts
@@ -1,3 +1,9 @@
-export default function () {
+import { NextFunction, Request, Response } from 'express';
+
+export default function (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) {
   return;
 }
